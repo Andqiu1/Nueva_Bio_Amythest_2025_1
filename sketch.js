d3fd7ec027ab36a98bdf435cf3fab186be445d94
Andqiu1 <andreqiu99@gmail.com>
2025-10-22 10:47:18 -0700

    Make canvas responsive to window size

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ var mouseClick=false
 var objects=[]
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES)
   background(22)
-  objects[0]= new object("hi",200,200,20,20,true,[50,100,70])
+  objects[0]= new object("hi",width/2,height/2,20,20,true,[50,100,70])
 }
 
 
